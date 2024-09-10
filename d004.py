65388707a90242f5fcda110cf88808c9f1381513
Kaích Henrique Costa <kaichhc@gmail.com>
2024-09-10 20:40:06 -0300

    fiz o d028

--- d004.py
+++ d004.py
@@ -7,4 +7,17 @@ print('É um número? ',valor.isnumeric())
 print('É composto apenas por espaços?',valor.isspace())
 print('É um número decimal? ',valor.isdecimal()) 
 print('É um caractere composto apenas por letras minúsculas?',valor.islower())
-print('É um caractere composto apenas por letras maiúsculas? ',valor.isupper())       
+print('É um caractere composto apenas por letras maiúsculas? ',valor.isupper())      
+
+try:
+
+    correto=4
+#==================================
+    tentativa = int(input('Tente adivinhar o número que estou pensando de 1 a 5'))
+    if tentativa==correto:
+        print('Parabéns, você acertou o número')
+    else:
+        print('Errou! Tente novamente')
+    print('---FIM---') 
+except:ValueError 
+print('Digite um número inteiro por favor') 
