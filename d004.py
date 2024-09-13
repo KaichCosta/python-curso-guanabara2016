6589a179ca2d589a8f748c1303f2de2e410d8b1e
Kaích Henrique Costa <kaichhc@gmail.com>
2024-09-13 20:13:28 -0300

    fiz até o 032

--- d004.py
+++ d004.py
@@ -9,15 +9,39 @@ print('É um número decimal? ',valor.isdecimal())
 print('É um caractere composto apenas por letras minúsculas?',valor.islower())
 print('É um caractere composto apenas por letras maiúsculas? ',valor.isupper())      
 
-try:
 
-    correto=4
+
 #==================================
-    tentativa = int(input('Tente adivinhar o número que estou pensando de 1 a 5'))
-    if tentativa==correto:
+
+from random import randint
+maquina = randint(0,5)
+try: 
+
+#==================================      
+    tentativa = int(input('Tente adivinhar o número que estou pensando de 1 a 5 = '))
+    print('Tente adivinhar o número que estou pensando de 1 a 5 = ')
+    if tentativa==maquina:
         print('Parabéns, você acertou o número')
     else:
-        print('Errou! Tente novamente')
+        print('Errou! Eu pensei em {}Tente novamente'.format(maquina))
     print('---FIM---') 
 except:ValueError 
 print('Digite um número inteiro por favor') 
+
+print('Escolha 3 números')
+n1=int(input('Número 1 = '))
+n2=int(input('Número 2 = '))
+n3=int(input('Número 3 = '))
+maior= 0
+menor= 0
+if n2 > n1:
+    n2 = maior
+else:
+    n1 = maior
+if n3 > n2: 
+    n3 > maior
+    n1 = menor
+else:
+    n1 > n3
+    n3 = menor
+print('O maior é {} e o menor é {}'.format(maior, menor))
