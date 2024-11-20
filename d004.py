cf8bffc23feb6a26f491ada85f3c583a66a04206
Kaích Henrique Costa <kaichhc@gmail.com>
2024-11-20 00:40:24 -0300

    SÓ ficou com um erro de validação no par ou impar no d068

--- d004.py
+++ d004.py
@@ -12,3 +12,45 @@ print('É um caractere composto apenas por letras maiúsculas? ',valor.isupper()
 
 
 #==================================
+from random import randint
+maq = randint (0,10)
+print('=='*30)
+print('|BEM VINDO AO JOGO DE PAR OU IMPAR|')
+print('=='*30)
+soma = 0
+resp = 'V'
+while resp == 'V':
+    escolha = (input('Digite ( P ) para PAR E ( I ) para ÍMPAR = ')).upper()
+    print(f'VC ESCOLHEU {escolha}')
+
+    if escolha != 'I' != 'P':
+
+        while escolha != 'I' != 'P':
+            print('DIGITE UMA ESCOLHA VÁLIDA')
+            escolha = (input('Digite ( P ) para PAR E ( I ) para ÍMPAR = ')).upper()
+            print(escolha)
+
+    n = int(input('Digite um valor = '))
+
+    valida = maq + n
+    if  valida %2 == 0 and escolha == 'P':
+        print('GANHOU A || PARTIDA {soma} !!! VAMOS DE NOVO ')
+        resp = 'V'
+        soma += 1
+        
+    if valida %2 == 0 and escolha == 'I':
+        print('FIM DE JOGO VC PERDEU')
+        break
+
+    if valida %2 != 0 and escolha == 'P':
+        print('FIM DE JOGO VC PERDEU')
+        break
+
+    if valida %2 != 0 and escolha == 'I':
+        soma += 1
+        print(f'GANHOU A || PARTIDA {soma} !!! VAMOS DE NOVO ')
+        resp = 'V'
+        
+print('=='*30)
+if soma >= 1:
+    print(f' FIM DE JOGO VC PERDEU !\n MAS GANHOU {soma} VEZES')
