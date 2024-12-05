fa13bb05769bddfd51d14428ffa73eb4ecb78493
Kaích Henrique Costa <kaichhc@gmail.com>
2024-12-04 23:55:55 -0300

    apaguei arquivos

--- TESTES.py
+++
@@ -1,5 +0,0 @@
-n= int(input('Digite o número que deve ser convertido = '))
-escolha= input('Digite 1) PARA BINÁRIO / 2) PARA OCTAL / 3) PARA HEXADECIMAL')
-
-if escolha == 1:
-    print('Binário = {0:b}'.format(n))
